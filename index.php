f57506cf0120984bcb16c699b7535b17c3362605
VIN-PC\vince <99008212@mydavinci.nl>
2017-12-12 11:54:24 +0100

    Finish

--- index.php
+++ index.php
@@ -1,76 +1,47 @@
+<?php
+		//date_default_timezone_set('CET');
+		$morning = 6;
+		$afternoon = 12;
+		$evening = 18;
+		$night= 0;
+
+    	$hour = date("H:i", time());
+
+	
+ 		if($hour >= $morning && $hour <= $afternoon){
+			$class = "morning";
+			echo "<p>Goede Morgen <br>Het is nu:\n" . $hour . "</p>" ;
+    	}
+
+    	if($hour >= $afternoon && $hour <= $evening){
+    		$class = "afternoon";
+    		echo "<p>Goede Middag <br>Het is nu:\n" . $hour . "</p>";
+    	}
+
+    	if($hour >= $evening && $hour <= $night){
+    		$class = "evening";
+    		echo "<p>Goede Avond <br>Het is nu:\n" . $hour . "</p>";
+    	}
+
+    	if($hour >= $night && $hour <= $morning){
+    		$class = "night";
+    		echo "<p>Goede Nacht <br>Het is nu:\n" . $hour . "</p>";
+    	}
+
+		?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Goede</title>
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
+<body class="<?=$class?>">
+
 	
-	<?php
-		include 'index.html';
-		//date_default_timezone_set('CET');
-		$morning = "6:00";
-		$afternoon = "12:00";
-		$evening = "18:00";
-		$night= "24:00";
-
-    	$hour = date("H", time());
-
-		if($hour > 10)
-		{
- 		if($hour >= $morning && $hour <= $afternoon){
-			echo date("H:i", time());
-
-    		echo  '<img src="img/morning.png" class="image">';
-    		
-
-    		/*$doc->loadHTML($html);
-			//get the element you want to append to
-			$descBox = $doc->getElementById('one');
-			//create the element to append to #element1
-			$appended = $doc->createElement('p', 'Het is nu ' + $hour + '!');*/
-    	}
-
-    	if($hour >= $afternoon && $hour <= $evening){
-    		echo date("H:i", time());
-
-    		echo '<img src="img/afternoon.png" class="image">';
-    		
-
-    		/*$doc->loadHTML($html);
-			//get the element you want to append to
-			$descBox = $doc->getElementById('one');
-			//create the element to append to #element1
-			$appended = $doc->createElement('p', 'Het is nu ' + $hour + '!');*/
-    	}
-
-    	if($hour >= $evening && $hour <= $night){
-    		echo date("H:i", time());
-
-    		echo'<img src="img/evening.png" class="image">';
-    		
-
-    		/*$doc->loadHTML($html);
-			//get the element you want to append to
-			$descBox = $doc->getElementById('one');
-			//create the element to append to #element1
-			$appended = $doc->createElement('p', 'Het is nu ' + $hour + '!');*/
-    	}
-
-    	if ($hour >= $night && $hour <= $morning){
-    		echo date("H:i", time());
-
-    		echo'<img src="img/night.png" class="image">';
-    		
-
-    		/*$doc->loadHTML($html);
-			//get the element you want to append to
-			$descBox = $doc->getElementById('one');
-			//create the element to append to #element1
-			$appended = $doc->createElement('p', 'Het is nu ' + $hour + '!');*/
-    	}
-    }
-
-		?>
+	
 
 </body>
 </html>
